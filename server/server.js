b531454d1f64b577c4d573fe70b50accbc4bd137
Robert Augusta <RobAAugusta@gmail.com>
2022-02-22 18:13:56 -0500

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGODB_URI;
 
 const server = new ApolloServer({
   typeDefs,
